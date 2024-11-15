1a965cef1bb75be1d6eccaf21efde71f1beed814
guiemiliano <guiemiliano.dev@gmail.com>
2024-11-15 18:04:05 -0300

    Módulo de introdução a OOP

--- OOP/src/sistemabancario/Banco.java
+++ OOP/src/sistemabancario/Banco.java
@@ -11,9 +11,23 @@ public class Banco {
     void sacar(Conta conta, double valor){
         double saldoAtual = conta.saldo;
         double novoSaldo = saldoAtual - valor;
+
+        if(novoSaldo < 0){
+            System.out.println("Não foi possível sacar, saldo insuficiente");
+        }else{
+            conta.saldo = novoSaldo;
+        }
     }
 
     void transferir(Conta origem, Conta destino, double valor){
-
+        double saldoAtualOrigem = origem.saldo;
+        double novoSaldoOrigem = saldoAtualOrigem - valor;
+        if(novoSaldoOrigem < 0){
+            System.out.println("Não foi possível transferir, saldo insuficiente");
+        }else{
+            origem.saldo = novoSaldoOrigem;
+            double novoSaldoDestino = destino.saldo + valor;
+            destino.saldo = novoSaldoDestino;
+        }
     }
 }
