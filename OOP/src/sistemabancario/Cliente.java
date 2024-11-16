6d6920dd659104b70ec053c43a5870852bbd3e7d
guiemiliano <guiemiliano.dev@gmail.com>
2024-11-16 19:50:32 -0300

    Módulo de introdução a OOP

--- OOP/src/sistemabancario/Cliente.java
+++ OOP/src/sistemabancario/Cliente.java
@@ -1,8 +1,16 @@
 package sistemabancario;
 
+/*
+   Construtores são métodos especiais usados para inicializar
+   objetos de uma classe, por padrão recebo um construtor sem parâmetros
+   ele tem o mesmo nome da classe
+ */
 public class Cliente {
-
     String nome;
     String cpf;
 
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
 }
