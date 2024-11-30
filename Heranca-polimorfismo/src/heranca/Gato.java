a296742d417a1096b79bad53159cb77270afad54
guiemiliano <guiemiliano.dev@gmail.com>
2024-11-30 14:30:49 -0300

    Aprendendo mais sobre Herança e Polimorfismo!

--- Heranca-polimorfismo/src/heranca/Gato.java
+++ Heranca-polimorfismo/src/heranca/Gato.java
@@ -1,10 +1,18 @@
+package heranca;
+
 public class Gato extends Animal{
+
+
+    public Gato(String nome, int idade) {
+        super("Gato", idade);
+    }
 
     public void miar(){
         System.out.println(getNome() + " está miando");
     }
 
     public void subirEmTelhado(){
+
         System.out.println(getNome() + " está subindo no telhado");
     }
 
