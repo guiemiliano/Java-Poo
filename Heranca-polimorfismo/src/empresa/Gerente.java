06a48cbe1ee7eeb37841415afe4ee5e99413aa21
guiemiliano <guiemiliano.dev@gmail.com>
2024-12-01 22:27:43 -0300

    Aprendendo mais sobre Herança e Polimorfismo!

--- Heranca-polimorfismo/src/empresa/Gerente.java
+++ Heranca-polimorfismo/src/empresa/Gerente.java
@@ -2,6 +2,8 @@ package empresa;
 
 public class Gerente extends Funcionario {
 
+
+    //Sobrescrita de método
     @Override
     public double calcularBonificacao() {
         double bonificacaoPadrao = super.calcularBonificacao();
